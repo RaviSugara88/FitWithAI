7b9b6b4779821245676795d75fd9321ae330cd5b
RaviSugara88 <ravi88sugra@gmail.com>
2026-02-16 16:51:29 +0530

    update Application Class

--- app/src/main/java/com/fitwithai/app/FitWithAiApp.kt
+++ app/src/main/java/com/fitwithai/app/FitWithAiApp.kt
@@ -1,13 +1,5 @@
 package com.fitwithai.app
 
 import android.app.Application
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.runtime.Composable
 
-class FitWithAiApp(): Application() {
-    override fun onCreate() {
-        super.onCreate()
-
-    }
-}
+class FitWithAiApp : Application()
