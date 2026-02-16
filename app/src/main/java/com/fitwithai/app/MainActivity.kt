f270c546ef4d54e63211e921de4e7f2706bb36f2
RaviSugara88 <ravi88sugra@gmail.com>
2026-02-16 16:52:30 +0530

    UpdateRoot

--- app/src/main/java/com/fitwithai/app/MainActivity.kt
+++ app/src/main/java/com/fitwithai/app/MainActivity.kt
@@ -3,14 +3,13 @@ package com.fitwithai.app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.fitwithai.ui.navigation.AppNavGraph
 import com.fitwithai.ui.screens.DashboardScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AppNavGraph()
+            DashboardScreen()
         }
     }
 }
