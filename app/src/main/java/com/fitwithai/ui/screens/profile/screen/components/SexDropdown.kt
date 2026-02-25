678295afd6940417a141c70d23574f0b43d6ea12
RaviSugara88 <ravi88sugra@gmail.com>
2026-02-25 15:07:21 +0530

    SexDropdown.kt

---
+++ app/src/main/java/com/fitwithai/ui/screens/profile/screen/components/SexDropdown.kt
@@ -0,0 +1,69 @@
+package com.fitwithai.ui.screens.profile.screen.components
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import com.fitwithai.ui.screens.profile.state.Sex
+
+@Composable
+fun SexDropdown(
+    selectedSex: Sex?,
+    onSexSelected: (Sex) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    var expanded by remember { mutableStateOf(false) }
+
+    Column(modifier = modifier.fillMaxWidth()) {
+
+        OutlinedTextField(
+            value = selectedSex?.displayName ?: "",
+            onValueChange = {},
+            enabled = false,
+            label = { Text("Sex") },
+            trailingIcon = {
+                androidx.compose.material3.Icon(
+                    imageVector = Icons.Default.ArrowDropDown,
+                    contentDescription = null,
+                    tint = Color.Black
+                )
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .clickable { expanded = true },
+            colors = OutlinedTextFieldDefaults.colors(
+                focusedBorderColor = Color.Green,
+                unfocusedBorderColor = Color.Green,
+                focusedTextColor = Color.White,
+                unfocusedTextColor = Color.White
+            )
+        )
+
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false }
+        ) {
+            Sex.values().forEach { sex ->
+                DropdownMenuItem(
+                    text = { Text(sex.displayName) },
+                    onClick = {
+                        expanded = false
+                        onSexSelected(sex)
+                    }
+                )
+            }
+        }
+    }
+}
