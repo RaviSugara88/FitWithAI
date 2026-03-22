10f1e42416ef23901ce9cca0fbd018250f4b6027
RaviSugara88 <ravi88sugra@gmail.com>
2026-03-22 13:56:55 +0530

    add KoinDI FitWithAiApp.kt

--- app/src/main/java/com/fitwithai/app/FitWithAiApp.kt
+++ app/src/main/java/com/fitwithai/app/FitWithAiApp.kt
@@ -1,5 +1,16 @@
 package com.fitwithai.app
 
 import android.app.Application
+import com.fitwithai.di.appModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
 
-class FitWithAiApp : Application()
+class FitWithAiApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidContext(this@FitWithAiApp)
+            modules(appModule)
+        }
+    }
+}
