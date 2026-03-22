f842d0169ac208c0eefb43951bdf2dbbffd7315c
RaviSugara88 <ravi88sugra@gmail.com>
2026-03-22 13:58:26 +0530

    updateNav

--- app/src/main/java/com/fitwithai/app/MainActivity.kt
+++ app/src/main/java/com/fitwithai/app/MainActivity.kt
@@ -1,26 +1,25 @@
 package com.fitwithai.app
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
-import com.fitwithai.ui.screens.profile.screen.ProfileScreen
-import com.fitwithai.ui.screens.profile.viewmodel.ProfileViewModel
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.activity.ComponentActivity
+import com.fitwithai.ui.navigation.AppNavGraph
+import com.fitwithai.ui.theme.FitWithAITheme
 
 class MainActivity : ComponentActivity() {
-    private val counterViewModel: ProfileViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
-            ProfileScreen(viewModel = counterViewModel)
-           // DashboardScreen()
-            // Dipti
-            // FixCost -
-           /* AppNavGraph(
-                onGoogleLogin = { onSuccess -> onSuccess() },
-                onInstagramLogin = { onSuccess -> onSuccess() },
-            )*/
-
+            FitWithAITheme {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    AppNavGraph()
+                }
+            }
         }
     }
 }
